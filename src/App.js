1ad22e392629912239d93cdba4b06657c3ad80f5
jossparling <jossparling@gmail.com>
2022-09-15 12:50:23 -0500

    passing tests about

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
